17600c171ce5272d39a3619568aab39d0c56263f
CHARLES <zhucekid@gmail.com>
2016-02-18 16:40:57 +0800

    Update rail_pitch.rb
    v1 ok

--- rail_pitch.rb
+++ rail_pitch.rb
@@ -24,19 +24,28 @@ def bracket_pitch(rail_tr, rail_trp=rail_pitch(rail_tr))
 	bracket_g = 0
 		rail_trp.each do |r|
 		    rail_g = rail_g + r
-			while rail_g - bracket_g > r/2
+		    while rail_g - bracket_g > r/2 && (r%2500 == 0)
 				if r == 2500 
 					bracket_trp << 1500
 					bracket_trp << 1000
 				else bracket_trp << 2500
 				end
 				bracket_g = eval bracket_trp.join('+')
+				puts "----"
+				print "rail_g = "+rail_g.to_s + "\n"
+				print "bracket_g = "+bracket_g.to_s + "\n"
 			end
 			
 		end
-		bracket_trp << (bracket_tr - bracket_g)
+
+		blft1 = (bracket_tr - bracket_g)/2
+		bracket_g = bracket_g + blft1
+		blft2 = bracket_tr - bracket_g
+		bracket_trp << blft1
+		bracket_trp << blft2
 end
 
+#test
 #rail_tr = 10700
 #rail_trp=rail_pitch(rail_tr)
 #print rail_trp
